c5c3e975799db069670e6c31f25ebe648efcc82b
Ben Collins <bcollins@bluecherry.net>
2010-11-10 10:27:08 -0500

    core: Remove reg_io_lock
    Not sure why this was needed, but reduced to just readl/writel
    
    HOWEVER, the reg_write function still does a read from the PCI_STATUS reg
    which keeps us from hanging on module load. Not idea why it's needed :/
    
    Signed-off-by: Ben Collins <bcollins@bluecherry.net>

--- solo6010.h
+++ solo6010.h
@@ -183,7 +183,6 @@ struct solo6010_dev {
 	int			nr_ext;
 	u32			irq_mask;
 	u32			motion_mask;
-	spinlock_t		reg_io_lock;
 
 	/* tw28xx accounting */
 	u8			tw2865, tw2864, tw2815;
@@ -234,36 +233,17 @@ struct solo6010_dev {
 
 static inline u32 solo_reg_read(struct solo6010_dev *solo_dev, int reg)
 {
-	unsigned long flags;
-	u32 ret;
-	u16 val;
-
-	spin_lock_irqsave(&solo_dev->reg_io_lock, flags);
-
-	ret = readl(solo_dev->reg_base + reg);
-	rmb();
-	pci_read_config_word(solo_dev->pdev, PCI_STATUS, &val);
-	rmb();
-
-	spin_unlock_irqrestore(&solo_dev->reg_io_lock, flags);
-
-	return ret;
+	return readl(solo_dev->reg_base + reg);
 }
 
+/* No effing clue why we need to read the pci status, but if we don't,
+ * we get some nasty hangs on module load. -- BenC */
 static inline void solo_reg_write(struct solo6010_dev *solo_dev, int reg,
 				      u32 data)
 {
-	unsigned long flags;
 	u16 val;
-
-	spin_lock_irqsave(&solo_dev->reg_io_lock, flags);
-
 	writel(data, solo_dev->reg_base + reg);
-	wmb();
 	pci_read_config_word(solo_dev->pdev, PCI_STATUS, &val);
-	rmb();
-
-	spin_unlock_irqrestore(&solo_dev->reg_io_lock, flags);
 }
 
 void solo6010_irq_on(struct solo6010_dev *solo_dev, u32 mask);
