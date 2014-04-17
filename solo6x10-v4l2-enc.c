bluecherrydvr/solo6x10

commit fb83a50f4925ad10e6416f3fbeb03492fce0ba8c

Fix *_s_std prototype

solo6x10-v4l2-enc.c
@@ -1289,7 +1289,7 @@ static int solo_enc_streamoff(struct file *file, void *priv,
 	return ret;
 }
 
-static int solo_enc_s_std(struct file *file, void *priv, v4l2_std_id *i)
+static int solo_enc_s_std(struct file *file, void *priv, v4l2_std_id i)
 {
 	return 0;
 }

solo6x10-v4l2.c
@@ -702,7 +702,7 @@ static int solo_streamoff(struct file *file, void *priv, enum v4l2_buf_type i)
 	return videobuf_streamoff(&fh->vidq);
 }
 
-static int solo_s_std(struct file *file, void *priv, v4l2_std_id *i)
+static int solo_s_std(struct file *file, void *priv, v4l2_std_id i)
 {
 	return 0;
 }

commit f2062ab0f9deb67312c89ed0d7daca99b7e8ef58

Fix build for 3.8 and earlier
Issue introduced by fb83a50.

solo6x10-v4l2-enc.c
@@ -1289,7 +1289,12 @@ static int solo_enc_streamoff(struct file *file, void *priv,
 	return ret;
 }
 
-static int solo_enc_s_std(struct file *file, void *priv, v4l2_std_id i)
+static
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(3, 9, 0)
+int solo_enc_s_std(struct file *file, void *priv, v4l2_std_id i)
+#else
+int solo_enc_s_std(struct file *file, void *priv, v4l2_std_id *i)
+#endif
 {
 	return 0;
 }

solo6x10-v4l2.c
@@ -702,7 +702,12 @@ static int solo_streamoff(struct file *file, void *priv, enum v4l2_buf_type i)
 	return videobuf_streamoff(&fh->vidq);
 }
 
-static int solo_s_std(struct file *file, void *priv, v4l2_std_id i)
+static
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(3, 9, 0)
+int solo_s_std(struct file *file, void *priv, v4l2_std_id i)
+#else
+int solo_s_std(struct file *file, void *priv, v4l2_std_id *i)
+#endif
 {
 	return 0;
 }